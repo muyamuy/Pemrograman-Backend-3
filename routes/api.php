a6b83e71b21f833c735a381ac2458bd7b000ed68
muyamuy <akumuyas32@gmail.com>
2023-10-30 09:13:32 +0700

    Pertemuan 5

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,4 +30,18 @@ Route::post('/animals', [AnimalController::class, 'store']);
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 // Method delete
-Route::delete('/animals/{id}', [AnimalController::class,'destroy']);
+Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+// Route menampilkan semua siswa 
+Route::get("students", [StudentController::class, "index"]);
+
+//membuat route method post
+Route::post('/students', [StudentController::class, 'store']);
+
+
+//Route method update
+Route::put('/students/{id}', [StudentController::class, 'update']);
+
+//Route method Delete
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+
