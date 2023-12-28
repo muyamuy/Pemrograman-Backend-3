23e55b4222f8c53164b9779bcc33c05d6b202f7f
muyamuy <akumuyas32@gmail.com>
2023-12-28 20:00:35 +0700

    pertemuan12

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,56 +1,68 @@
-// TODO 3: Import data students dari folder data/students.js
-const students = require('../data/students.js');
+// import Model Student
+const Student = require("../models/Student");
 
-// Membuat Class StudentController
 class StudentController {
-    index(req, res) {
-      // TODO 4: Tampilkan data students
-      const data = {
-            message: "Menampilkan semua students",
-            data: [students],
-        };
+  // menambahkan keyword async
+  async index(req, res) {
+    // memanggil method static all dengan async await.
+    const students = await Student.all();
 
-        res.json(data);
-    }
-  
-    store(req, res) {
-      // TODO 5: Tambahkan data students
-      const {nama} = req.body;
-      students.push(nama);
-      const data = {
-            message: `Menambahkan data student : ${nama}`,
-            data: [students],
-        };
+    const data = {
+      message: "Menampilkkan semua students",
+      data: students,
+    };
 
-        res.json(data);
-    }
-  
-    update(req, res) {
-      // TODO 6: Update data students
-      const {id} = req.params;
-      const {nama} = req.body;
-      students[id] = nama;
-      const data = {
-            message: `Mengedit student id ${id}, nama : ${nama}`,
-            data: [students],
-        };
-        res.json(data);
-    }
-  
-    destroy(req, res) {
-      // TODO 7: Hapus data students
-      const {id} = req.params;
-      students.splice(id, 1);
-      const data = {
-            message: `Menghapus student id ${id}`,
-            data: [students],
-        };
-        res.json(data);
-    }
+    res.json(data);
   }
-  
-  // Membuat object StudentController
-  const object = new StudentController();
-  
-  // Export object StudentController
-  module.exports = object;
+
+ async store(req, res) {
+     /**
+     * TODO 2: memanggil method create.
+     * Method create mengembalikan data yang baru diinsert.
+     * Mengembalikan response dalam bentuk json.
+     */
+    // code here
+    try {
+        const newStudent = await Student.create(req.body);
+
+        const data = {
+            message: "Menambahkan data student",
+            data: newStudent,
+        };
+
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Terjadi kesalahan pada server" });
+    }
+}
+
+  update(req, res) {
+    const { id } = req.params;
+    const { nama } = req.body;
+
+    const data = {
+      message: `Mengedit student id ${id}, nama ${nama}`,
+      data: [],
+    };
+
+    res.json(data);
+  }
+
+  destroy(req, res) {
+    const { id } = req.params;
+
+    const data = {
+      message: `Menghapus student id ${id}`,
+      data: [],
+    };
+
+    res.json(data);
+  }
+}
+
+// Membuat object StudentController
+const object = new StudentController();
+
+// Export object StudentController
+module.exports = object;
--- data/students.js
+++
@@ -1,5 +0,0 @@
-// TODO 1: Buat data students
-const students = ["Hilmi", "Sabda", "Fatih"];
-
-// TODO 2: export data students
-module.exports = students;
